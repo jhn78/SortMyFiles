4c62e565f288f33083c06980e7d2878f5e49787c
jhn78 <jan.hoffmann.privat@gmail.com>
2017-04-22 15:13:01 +0200

    + simple Queue

---
+++ Queue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortMyFiles
+{
+    public class Queue
+    {
+        private Subject<object> _inner = new Subject<object>();
+
+        public IObservable<T> Register<T>()
+        {
+            return _inner.OfType<T>().Publish().RefCount();
+        }
+
+        public void Publish<T>(T message)
+        {
+            _inner.OnNext(message);
+        }
+    }
+}
